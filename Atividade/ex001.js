b5c2251a744171965f4c2876b2d988161203d50b
Winsantos <wingridnicoly19@gmail.com>
2026-04-15 16:53:44 -0300

    Add files via upload

---
+++ Atividade/ex001.js
@@ -0,0 +1,15 @@
+let nome = prompt(`Escreva o seu nome`)
+let ano = +prompt(`Coloque o ano da sua matrícula`)
+let NovaMatricula = 0
+
+
+console.log("Sistemas de novas matrículas do ano de 2026")
+
+while(matricula <= 2610){
+    console.log(`Atualização de novas matrículas:
+        ${matricula}
+        `)
+        matricula++
+        alert("Seja bem-vindo(a) à nossa academia!")
+}
+
