e32c464828989b8c00ed7f9a99022e04adc34458
Winsantos <wingridnicoly19@gmail.com>
2026-05-04 17:16:30 -0300

    Add files via upload

---
+++ nicoly/04/05/script.js
@@ -0,0 +1,3 @@
+const banner = document.querySelector('.app__image')
+const titulos = document.querySelector('.app__title')
+const html = document.documentElement /* Trazendo elemento html fora do body*/ 
